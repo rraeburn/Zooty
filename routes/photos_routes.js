423ae17fdb5bc54d576e716b22853c010cf838e3
callumsong <ghksong@gmail.com>
2015-02-26 11:56:00 -0800

    trying once again

--- routes/photos_routes.js
+++ routes/photos_routes.js
@@ -11,7 +11,7 @@ module.exports = function(app, appSecret) {
   app.post('/upload', eat_auth.validateToken(appSecret), function(req,res) {
     var newPhoto = new Photo();
     newPhoto.phoneId = req.phoneId;
-    fs.writeFileSync('./public/'+ newPhoto._id + '.png', req.body.photoFile, 'utf8');
+    fs.writeFileSync('./public/'+ newPhoto._id + '.png', req.body.photoFile);
     newPhoto.photoUrl = 'http://zooty.herokuapp.com/' + newPhoto._id + '.png';
     newPhoto.save(function(err, data) {
       if(err) return res.status(500).send({msg: 'could not upload photo'});
